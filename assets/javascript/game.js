ad061421bc5b862b16550ec9032960c13e61a925
Seana Curran <shaunacurran@shaunas-mbp.attlocal.net>
2017-05-21 23:53:40 -0500

    update game

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesLeft = 10;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log(computerChoice)
+
+
+
+   // When the user presses a key, it will run the following function...
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+
+    if(userGuess === computerChoice){
+        wins++;
+    }
+    else{
+        guesses--;
+    }
+
+    if(guesses === 0){
+        losses++
+    }
+
+  
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
+    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+    document.getElementById('guessedLetters').innerHTML = "Guesses So Far" + userGuess;
+
+
+
+};
